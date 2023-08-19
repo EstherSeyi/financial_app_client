52e7b0464589e4bfac3527da74efc5e5feaa789e
Esther Ogundijo <sheyiogundijo@gmail.com>
2023-08-20 00:55:45 +0100

    adds search functionality

--- src/components/SearchCity.tsx
+++ src/components/SearchCity.tsx
@@ -1,10 +1,98 @@
+import { Fragment, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { Combobox, Transition } from "@headlessui/react";
+import { CheckIcon } from "lucide-react";
+import { useSearchCityByName } from "../hooks/city";
+import { City } from "../types";
+
 const SearchCity = () => {
+  const [selected, setSelected] = useState<City | null>(null);
+  const [query, setQuery] = useState("");
+  const { data, isLoading, isError } = useSearchCityByName(query);
+  const navigate = useNavigate();
+
+  const handleSelect = (selected: City) => {
+    setSelected(selected);
+    navigate(
+      `/${selected.name}?lat=${selected.latitude}&lon=${selected.longitude}`
+    );
+    setSelected(null);
+  };
+
   return (
-    <div>
-      <input
-        className="w-full bg-[#202b3b] p-2 rounded-lg"
-        placeholder="Search for cities..."
-      />
+    <div className="w-72">
+      <Combobox value={selected} onChange={handleSelect}>
+        <div className="relative mt-1">
+          <div className="relative w-full cursor-default overflow-hidden rounded-lg bg-white text-left shadow-md focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-highlightBlue sm:text-sm">
+            <Combobox.Input
+              placeholder="Search By City Name..."
+              className="w-full border-none py-2 pl-3 pr-10 text-sm leading-5 text-gray-900 focus:ring-highlightBlue"
+              displayValue={(city: City) => city?.name}
+              onChange={(event) => setQuery(event.target.value)}
+            />
+          </div>
+          <Transition
+            as={Fragment}
+            leave="transition ease-in duration-100"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+            afterLeave={() => setQuery("")}
+          >
+            <Combobox.Options className="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-[#202b3b] py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm border border-highlightBlue z-[1]">
+              {isLoading ? (
+                <div>Loading...</div>
+              ) : isError ? (
+                <div>Errored</div>
+              ) : !data || data.length === 0 ? (
+                <div className="relative cursor-default select-none py-2 px-4 text-white">
+                  No City Found.
+                </div>
+              ) : (
+                data.map((city) => (
+                  <Combobox.Option
+                    key={`${city.latitude},${city.longitude}`}
+                    className={({ active }) =>
+                      `relative cursor-default select-none py-4 pl-10 pr-4 ${
+                        active
+                          ? "bg-highlightBlue text-white"
+                          : "text-[#9399a2]"
+                      }`
+                    }
+                    value={city}
+                  >
+                    {({ selected, active }) => (
+                      <>
+                        <span className="flex justify-between">
+                          <span>
+                            <span
+                              className={`block truncate text-2xl ${
+                                selected ? "font-medium" : "font-normal"
+                              }`}
+                            >
+                              {city.name}
+                            </span>
+                            <span>{city.country}</span>
+                          </span>
+                          <span className="text-xl">31°</span>
+                        </span>
+                        {selected ? (
+                          <span
+                            className={`absolute inset-y-0 left-0 flex items-center pl-3 ${
+                              active ? "text-white" : "text-teal-600"
+                            }`}
+                          >
+                            <CheckIcon className="h-5 w-5" aria-hidden="true" />
+                          </span>
+                        ) : null}
+                      </>
+                    )}
+                  </Combobox.Option>
+                ))
+              )}
+            </Combobox.Options>
+          </Transition>
+        </div>
+      </Combobox>
     </div>
   );
 };
